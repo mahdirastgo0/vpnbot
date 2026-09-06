2d225f6b69620ac259af1a62f5c20f709cafa361
mahdirastgo0 <rastgoom23@gmail.com>
2026-09-06 18:19:49 +0330

    sss

--- app/handlers/user/payment.py
+++ app/handlers/user/payment.py
@@ -76,7 +76,7 @@ async def pay_card(callback: CallbackQuery, session: AsyncSession, state: FSMCon
             holder=settings.CARD_HOLDER_NAME,
             bank=settings.CARD_BANK_NAME,
         ),
-        parse_mode="MarkdownV2",
+        parse_mode="HTML",
     )
     await callback.answer()
 
